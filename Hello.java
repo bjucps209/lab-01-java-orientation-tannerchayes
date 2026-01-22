92fd9d1ba37129cc9e7d821796135b8c34cf7806
Tanner <thaye915@students.bju.edu>
2026-01-21 22:35:32 -0500

    fixed code

--- Hello.java
+++ Hello.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 class Hello {
     public static void main(String[] args) {
-        Scanner scan = new Scanner();
-        int num1 = Interger.parseInt(scan.nextLine());
-        int num2 = Interger.parseInt(scan.nextLine());
-        System.out.println("The sum is: " + num1 + num2);
+        Scanner scan = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(scan.nextLine());
+        int num2 = Integer.parseInt(scan.nextLine());
+
+        System.out.println("The sum is: " + (num1 + num2));
     }
 }
