7091f1f056663865d0a546628b9ac7c8381873bf
Tanner <thaye915@students.bju.edu>
2026-01-21 21:55:29 -0500

    Typed in Listing one

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Hello {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner();
+        int num1 = Interger.parseInt(scan.nextLine());
+        int num2 = Interger.parseInt(scan.nextLine());
+        System.out.println("The sum is: " + num1 + num2);
+    }
+}
